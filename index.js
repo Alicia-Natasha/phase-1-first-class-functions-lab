7ede15c21adce900767bc5d9ccfda909c0002a39
Alicia-Natasha <alicia.natasha@student.moringaschool.com>
2025-03-05 21:14:59 +0300

    phase-1-first-class-functions-lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+const drivers = ([ 'Antonia', 'Nuru', 'Amari', 'Mo' ]);
+
+function returnFirstTwoDrivers(drivers) {
+    console.log(drivers);
+    return drivers.slice(0, 2);
+}
+
+function returnLastTwoDrivers(drivers) {
+    console.log(drivers);
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = ([returnFirstTwoDrivers, returnLastTwoDrivers ]);
+
+const createFareMultiplier = function(multiplier) {
+    return function(fare) {
+        return fare * multiplier;
+    };
+};
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function(drivers, driverSelector) {
+    return driverSelector(drivers);
+};
